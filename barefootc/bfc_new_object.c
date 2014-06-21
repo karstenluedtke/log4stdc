da70a33f5fbb7f6d8b9c03ebae1d4b1946ab1d8b
Karsten Lüdtke <k.luedtke@teles.de>
2014-06-21 13:47:55 +0000

    bfc_new() for loggers and appenders

--- barefootc/bfc_new_object.c
+++ barefootc/bfc_new_object.c
@@ -11,10 +11,15 @@ bfc_new(bfc_classptr_t cls, struct mempool *pool)
 
 	size = CMETHCALL(cls, clonesize, (NULL), 4*sizeof(struct bfc_objhdr));
 	buf  = mempool_alloc(pool, size);
-	if (buf) {
-		newobj = bfc_init_object(cls, buf, size, pool);
-		newobj->pool = pool;
+	if (buf == NULL) {
+		return (NULL);
 	}
+	newobj = bfc_init_object(cls, buf, size, pool);
+	if (newobj == NULL) {
+		mempool_free(pool, buf);
+		return (NULL);
+	}
+	newobj->pool = pool;
 	return (newobj);
 }
 

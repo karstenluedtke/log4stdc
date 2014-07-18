7728fa3b0396aa4432009a204d721aa626cb6bc3
Karsten Lüdtke <k.luedtke@teles.de>
2014-07-18 17:50:52 +0000

    added formatmsg

---
+++ log4stdc/l4sc_formatmsg.c
@@ -0,0 +1,14 @@
+
+#include <stddef.h>
+#include <stdlib.h>
+
+#include "logobjects.h"
+
+int
+l4sc_formatmsg(l4sc_layout_cptr_t layout,
+	  l4sc_logmessage_cptr_t msg, char *buf, size_t bufsize)
+{
+	RETURN_METHCALL(l4sc_layout_class_ptr_t, layout,
+			format, (layout,msg,buf,bufsize), 0);
+}
+

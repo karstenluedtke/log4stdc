214d5748f2e288fe9593997e935a91410a1a1187
Karsten Lüdtke <k.luedtke@teles.de>
2014-09-13 18:06:27 +0000

    atomic counter

---
+++ barefootc/atomic.h
@@ -0,0 +1,79 @@
+/**
+ * @file      atomic.h
+ *
+ * @brief     Simple synchronization within the barefootc library.
+ *
+ * @author    Karsten Luedtke
+ *
+ * @date      2014-09-13
+ *
+ * Copyright (c)  2014  Karsten Luedtke, Berlin, Germany.
+ */
+
+#ifndef _BFC_ATOMIC_H_
+#define _BFC_ATOMIC_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(__APPLE__)
+
+#include <inttypes.h>
+#include <libkern/OSAtomic.h>
+
+typedef volatile int32_t bfc_atomic_counter_t;
+
+#define bfc_init_atomic_counter(ctr,v)	ctr = (v)
+#define bfc_incr_atomic_counter(ctr)	OSAtomicIncrement32Barrier(&(ctr))
+#define bfc_decr_atomic_counter(ctr)	OSAtomicDecrement32Barrier(&(ctr))
+#define bfc_add_atomic_counter(ctr,v)	OSAtomicAdd32Barrier((v),&(ctr))
+
+#define bfc_decr_and_test_atomic_counter(ctr) \
+				(OSAtomicDecrement32Barrier(&(ctr)) == 0)
+
+#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 201112L) && !defined(__STDC_NO_ATOMICS__)
+
+#include <stdatomic.h>
+
+typedef atomic_int_least32_t bfc_atomic_counter_t;
+
+/*
+ * The atomic_fetch_xxx functions return the old value before modification
+ */
+#define bfc_init_atomic_counter(ctr,v)	atomic_init(&(ctr),(v))
+#define bfc_incr_atomic_counter(ctr)	(atomic_fetch_add(&(ctr),1) + 1)
+#define bfc_decr_atomic_counter(ctr)	(atomic_fetch_sub(&(ctr),1) - 1)
+#define bfc_add_atomic_counter(ctr,v)	(atomic_fetch_add(&(ctr),(v)) + (v))
+
+#define bfc_decr_and_test_atomic_counter(ctr) (atomic_fetch_sub(&(ctr),1) == 1)
+
+#elif defined(__i386) || defined(__i386__) || defined(__x86_64) || defined(__x86_64__) || defined(__amd64)
+
+typedef volatile int32_t bfc_atomic_counter_t;
+
+#define bfc_init_atomic_counter(ctr,v)	ctr = (v)
+
+#define bfc_incr_atomic_counter(ctr)	\
+	__asm__ __volatile__("lock incl %0" :"=m"(ctr) :"m"(ctr));
+
+#define bfc_decr_and_test_atomic_counter(ctr)	\
+	bfc_decr_and_test_i386_atomic(&(ctr))
+
+static inline int bfc_decr_and_test_i386_atomic(bfc_atomic_counter_t *cp)
+{
+	unsigned char c;
+
+	__asm__ __volatile__("lock decl %0; sete %1"	:"=m"(*cp), "=qm"(c)
+							:"m"(*cp) : "memory");
+	return (c);
+}
+
+#else
+
+#endif
+
+#ifdef __cplusplus
+}	/* C++ */
+#endif
+#endif /* _BFC_ATOMIC_H_ */
---
+++ tests/synchronization/atomic_counter.c
@@ -0,0 +1,56 @@
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <errno.h>
+
+#include "barefootc/atomic.h"
+#include "log4stdc.h"
+
+static l4sc_logger_ptr_t logger;
+
+static void 
+t1(int n)
+{
+	int i, rc;
+	bfc_atomic_counter_t ctr;
+
+	bfc_init_atomic_counter(ctr, 0);
+	assert (ctr == 0);
+
+	for (i=0; i < n; i++) {
+		bfc_incr_atomic_counter(ctr);
+	}
+
+	assert (ctr == n);
+
+	for (i=0; i < n-1; i++) {
+		rc = bfc_decr_and_test_atomic_counter(ctr);
+		assert (rc == 0);
+	}
+
+	if (n > 0) {
+		rc = bfc_decr_and_test_atomic_counter(ctr);
+		assert (rc == 1);
+	}
+
+	assert (ctr == 0);
+}
+
+int
+main (int argc, char *argv)
+{
+	int i, j;
+
+	l4sc_configure_from_xml_file("log4j.xml");
+	logger = l4sc_get_logger("barefootc", 0);
+
+	t1(0);
+	t1(1);
+	t1(2);
+	t1(15);
+	t1(65000);
+
+	return (0);
+}

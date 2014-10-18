8885110aa525221e2c188d68e5381d5dace64693
Karsten Lüdtke <k.luedtke@teles.de>
2014-10-18 11:43:50 +0000

    conditional for log4cxx replacement library

--- cf.sh
+++ cf.sh
@@ -1,3 +1,4 @@
 CPPFLAGS="-I../fmcc/ThirdParty/i386-linux-64/include"	\
 LDFLAGS="-L../fmcc/ThirdParty/i386-linux-64/lib"	\
-./configure --prefix $HOME/fmcc/ThirdParty/host-native
+./configure --prefix $HOME/fmcc/ThirdParty/host-native	\
+	--enable-bfclog4cxx=auto
--- testlink.cpp
+++ testlink.cpp
@@ -4,18 +4,26 @@
 
 #include "log4stdc.h"
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifdef HAVE_LOG4CXX_LOG4CXX_H
 #include <log4cxx/logger.h>
 #include "log4cxx/logmanager.h"
 #include "log4cxx/basicconfigurator.h"
 #include "log4cxx/propertyconfigurator.h"
 #include "log4cxx/xml/domconfigurator.h"
 #include "log4cxx/helpers/exception.h"
+#endif
 
 int main (int argc, char *argv[])
 {
 	l4sc_logger_ptr_t logger;
 
+#ifdef HAVE_LIBEXPAT
 	l4sc_configure_from_xml_file("log4j.xml");
+#endif
 	l4sc_get_root_logger();
 
 	logger = l4sc_get_logger("pal", 3);
@@ -28,6 +36,7 @@ int main (int argc, char *argv[])
 	l4sc_log(logger, INFO_LEVEL, "another line from main", 22,
 				__FILE__, __LINE__, __FUNCTION__);
 
+#ifdef HAVE_LOG4CXX_LOG4CXX_H
 	try {
 		log4cxx::BasicConfigurator::configure();
 
@@ -41,6 +50,7 @@ int main (int argc, char *argv[])
 	} catch(log4cxx::helpers::Exception&) {
 		return (1);
 	}
+#endif
 
 	L4SC_DEBUG(logger, "logger(\"pal\") = %p, %d, %d", logger, 1, 2);
 

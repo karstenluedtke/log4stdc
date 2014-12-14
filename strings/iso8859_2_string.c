7b35f07e58104f8dfbd83dfb65171cbfd32e7dd9
Karsten Lüdtke <k.luedtke@teles.de>
2014-12-14 20:14:25 +0000

    iso8859_1_string, iso8859_2_string, cp1250_string, cp1252_string

---
+++ strings/iso8859_2_string.c
@@ -0,0 +1,254 @@
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include <inttypes.h>
+
+#define NEED_BFC_STRING_CLASS 1
+
+#include "barefootc/object.h"
+#include "barefootc/string.h"
+#include "barefootc/mempool.h"
+#include "log4stdc.h"
+
+extern struct bfc_classhdr bfc_iso8859_2_traits_class;
+
+static long get_codept(bfc_cstrptr_t s, size_t pos);
+static int set_codept(bfc_strptr_t s, size_t pos, long c);
+static int init_begin_iterator(bfc_cstrptr_t s, bfc_iterptr_t it, size_t bs);
+static int init_limit_iterator(bfc_cstrptr_t s, bfc_iterptr_t it, size_t bs);
+static int init_rbegin_iterator(bfc_cstrptr_t s, bfc_iterptr_t it, size_t bs);
+static int init_rlimit_iterator(bfc_cstrptr_t s, bfc_iterptr_t it, size_t bs);
+
+static int mutable_substr(bfc_cstrptr_t s, size_t pos, size_t n,
+					void *buf, size_t bufsize);
+
+struct bfc_string_class bfc_shared_iso8859_2_string_class = {
+	.super 		= &bfc_shared_string_class,
+	.name 		= "shared_iso8859_2_string",
+	.init 		= bfc_init_empty_iso8859_2_string,
+
+	.getl		= get_codept,
+	.setl		= set_codept,
+
+	.ibegin		= init_begin_iterator,
+	.ilimit		= init_limit_iterator,
+	.rbegin		= init_rbegin_iterator,
+	.rlimit		= init_rlimit_iterator,
+
+	.traits		= (void *) &bfc_iso8859_2_traits_class,
+
+	.substr		= bfc_shared_iso8859_2_substr,
+	.mutable_substr	= mutable_substr,
+	.buffered_substr= bfc_buffered_iso8859_2_substr,
+};
+
+struct bfc_string_class bfc_buffered_iso8859_2_string_class = {
+	.super 		= &bfc_string_class,
+	.name 		= "buffered_iso8859_2_string",
+	.init 		= bfc_init_empty_iso8859_2_string,
+
+	.getl		= get_codept,
+	.setl		= set_codept,
+
+	.ibegin		= init_begin_iterator,
+	.ilimit		= init_limit_iterator,
+	.rbegin		= init_rbegin_iterator,
+	.rlimit		= init_rlimit_iterator,
+
+	.traits		= (void *) &bfc_iso8859_2_traits_class,
+
+	.substr		= bfc_shared_iso8859_2_substr,
+	.mutable_substr	= mutable_substr,
+	.buffered_substr= bfc_buffered_iso8859_2_substr
+};
+
+int
+bfc_init_empty_iso8859_2_string(void *buf, size_t bufsize,
+				  struct mempool *pool)
+{
+	static const long zbuf = 0;
+	l4sc_logger_ptr_t logger = l4sc_get_logger(BFC_STRING_LOGGER);
+
+	BFC_STRING_INIT_PROLOGUE(bfc_string_classptr_t,
+			  bfc_strptr_t, s, buf, bufsize, pool,
+			  &bfc_shared_iso8859_2_string_class);
+
+	L4SC_WARN(logger, "%s(%p, %ld, pool %p): default constructor called!",
+		__FUNCTION__, buf, (long) bufsize, pool);
+
+	s->buf = (void *) &zbuf;
+	return (BFC_SUCCESS);
+}
+
+int
+bfc_init_shared_iso8859_2_string(void *buf, size_t bufsize,
+				  const char *s, size_t n)
+{
+	l4sc_logger_ptr_t logger = l4sc_get_logger(BFC_STRING_LOGGER);
+
+	BFC_STRING_INIT_PROLOGUE(bfc_string_classptr_t,
+			  bfc_strptr_t, obj, buf, bufsize, NULL,
+			  &bfc_shared_iso8859_2_string_class);
+
+	L4SC_TRACE(logger, "%s(%p, %ld, s %p, n %ld)",
+			__FUNCTION__, buf, (long) bufsize, s, (long) n);
+
+	if (n >= bfc_basic_string_max_size(obj)) {
+		L4SC_ERROR(logger, "%s: too large string: %lu >= %lu chars",
+			__FUNCTION__, (unsigned long) n,
+			(unsigned long) bfc_basic_string_max_size(obj));
+		return (-EINVAL);
+	}
+
+	obj->buf = (void *) (intptr_t) s;
+	obj->bufsize = obj->len = n;
+	return (BFC_SUCCESS);
+}
+
+int
+bfc_init_shared_iso8859_2_string_c_str(void *buf,size_t bufsize,const char *s)
+{
+	int n;
+	l4sc_logger_ptr_t logger = l4sc_get_logger(BFC_STRING_LOGGER);
+
+	L4SC_TRACE(logger, "%s(%p, %ld, s %p)",
+		__FUNCTION__, buf, (long) bufsize, s);
+
+	n = (*bfc_shared_iso8859_2_string_class.traits->szlen)(s);
+	return bfc_init_shared_iso8859_2_string(buf, bufsize, s, n);
+}
+
+int
+bfc_init_buffered_iso8859_2_string(void *buf, size_t bufsize,
+				    const char *s, size_t n)
+{
+	l4sc_logger_ptr_t logger = l4sc_get_logger(BFC_STRING_LOGGER);
+
+	BFC_STRING_INIT_PROLOGUE(bfc_string_classptr_t,
+			  bfc_strptr_t, obj, buf, bufsize, NULL,
+			  &bfc_buffered_iso8859_2_string_class);
+
+	L4SC_TRACE(logger, "%s(%p, %ld, s %p, n %ld)",
+			__FUNCTION__, buf, (long) bufsize, s, (long) n);
+
+	if (n >= bfc_basic_string_max_size(obj)) {
+		L4SC_ERROR(logger, "%s: too large string: %lu >= %lu chars",
+			__FUNCTION__, (unsigned long) n,
+			(unsigned long) bfc_basic_string_max_size(obj));
+		return (-EINVAL);
+	}
+
+	obj->buf = (void *) (intptr_t) s;
+	obj->bufsize = obj->len = n;
+	return (BFC_SUCCESS);
+}
+
+int
+bfc_shared_iso8859_2_substr(bfc_cstrptr_t s, size_t pos, size_t n,
+						void *buf, size_t bufsize)
+{
+	const char *data = bfc_string_subdata(s, pos);
+	const size_t sublen = bfc_string_sublen(s, pos, n);
+
+	if ((pos == BFC_NPOS) || (pos > bfc_strlen(s))) {
+		return (-ERANGE);
+	}
+	return (bfc_init_shared_iso8859_2_string(buf, bufsize, data, sublen));
+}
+
+static int
+mutable_substr(bfc_cstrptr_t s, size_t pos, size_t n, void *buf, size_t bufsize)
+{
+	const char *data = bfc_string_subdata(s, pos);
+	const size_t sublen = bfc_string_sublen(s, pos, n);
+	l4sc_logger_ptr_t logger = l4sc_get_logger(BFC_STRING_LOGGER);
+
+	L4SC_ERROR(logger, "%s: not available in class %s",
+				__FUNCTION__, BFC_CLASS(s)->name);
+
+	bfc_init_shared_iso8859_2_string(buf, bufsize, data, sublen);
+	return (-ENOSYS);
+}
+
+int
+bfc_buffered_iso8859_2_substr(bfc_cstrptr_t s, size_t pos, size_t n,
+				void *buf, size_t bufsize,
+				void *databuf, size_t dbufsize)
+{
+	int rc;
+
+	if ((pos == BFC_NPOS) || (pos > bfc_strlen(s))) {
+		return (-ERANGE);
+	}
+	rc = bfc_init_buffered_iso8859_2_string(buf, bufsize,
+						 databuf, dbufsize);
+	if (rc >= 0) {
+		bfc_strptr_t substr = (bfc_strptr_t) buf;
+		const char *data = bfc_string_subdata(s, pos);
+		const size_t sublen = bfc_string_sublen(s, pos, n);
+		rc = bfc_string_assign_buffer(substr, data, sublen);
+	}
+	return (rc);
+}
+
+static long
+get_codept(bfc_cstrptr_t s, size_t pos)
+{
+	const char *p;
+	unsigned codept;
+
+	p = (char*) bfc_string_index((bfc_strptr_t)(uintptr_t)s, pos);
+	codept = (*s->vptr->traits->to_int)(*p);
+	return (codept);
+}
+
+static int
+set_codept(bfc_strptr_t s, size_t pos, long codept)
+{
+	char c = (*s->vptr->traits->to_char)((int)codept);
+
+	if ((pos == BFC_NPOS) || (pos > bfc_strlen(s))) {
+		return (-ERANGE);
+	} else if (pos == bfc_strlen(s)) {
+		bfc_string_push_back((bfc_strptr_t)s, c);
+	} else {
+		char *p = (char*)bfc_string_index((bfc_strptr_t)s, pos);
+		*p = c;
+	}
+	return (BFC_SUCCESS);
+}
+
+#define INIT_FORWARD_ITERATOR	bfc_init_iterator
+#define INIT_REVERSE_ITERATOR	bfc_init_reverse_iterator
+
+static int
+init_begin_iterator(bfc_cstrptr_t s, bfc_iterptr_t it, size_t bs)
+{
+	return (INIT_FORWARD_ITERATOR(it, bs, (bfc_cobjptr_t)s, 0));
+}
+
+static int
+init_limit_iterator(bfc_cstrptr_t s, bfc_iterptr_t it, size_t bs)
+{
+	size_t pos = bfc_strlen(s);
+	return (INIT_FORWARD_ITERATOR(it, bs, (bfc_cobjptr_t)s, pos));
+}
+
+static int
+init_rbegin_iterator(bfc_cstrptr_t s, bfc_iterptr_t it, size_t bs)
+{
+	size_t n = bfc_strlen(s);
+	size_t pos = (n > 0)? (n-1): BFC_NPOS;
+	return (INIT_REVERSE_ITERATOR(it, bs, (bfc_cobjptr_t)s, pos));
+}
+
+static int
+init_rlimit_iterator(bfc_cstrptr_t s, bfc_iterptr_t it, size_t bs)
+{
+	return (INIT_REVERSE_ITERATOR(it, bs, (bfc_cobjptr_t)s, BFC_NPOS));
+}
+
+
---
+++ tests/string.conversions/insert_size_cp1252_string.c
@@ -0,0 +1,173 @@
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wchar.h>
+#include <assert.h>
+
+#if defined(_WIN32) || defined(__MINGW32__) || defined(__MINGW64__)
+#include <malloc.h>  /* for alloca */
+#endif
+
+#include "log4stdc.h"
+#include "barefootc/string.h"
+#include "barefootc/mempool.h"
+
+static struct mempool *pool;
+static l4sc_logger_ptr_t logger;
+
+static int
+test(const char *s1, size_t pos1, const char *s2, const char *expected)
+{
+	int rc;
+	bfc_string_t s, ws;
+	size_t origlen, wslen, newlen;
+
+	L4SC_TRACE(logger, "%s(\"%s\", pos %ld, \"%s\", expected \"%s\")",
+		__FUNCTION__, s1, (long) pos1, s2, expected);
+
+	rc = bfc_init_basic_string_c_str(&s, sizeof(s), pool, s1);
+	assert(rc >= 0);
+
+	origlen = bfc_string_length(&s);
+	assert(origlen == strlen(s1));
+	
+	rc = bfc_init_shared_cp1252_string_c_str(&ws, sizeof(ws), s2);
+	assert(rc >= 0);
+
+	wslen = bfc_string_length(&ws);
+	assert(wslen == strlen(s2));
+	
+	rc = bfc_string_insert(&s, pos1, &ws);
+	newlen = bfc_string_length(&s);
+	if (rc >= 0) {
+        	assert(pos1 <= origlen);
+		assert(newlen >= origlen + wslen);
+		assert(bfc_string_compare_c_str(&s, expected) == 0);
+		assert(bfc_string_compare_c_str(&ws, s2) == 0);
+	} else {
+		assert(pos1 > origlen);
+		assert(newlen == origlen);
+		assert(bfc_string_compare_c_str(&s, s1) == 0);
+		assert(bfc_string_compare_c_str(&ws, s2) == 0);
+	}
+
+	bfc_destroy(&s);
+	bfc_destroy(&ws);
+
+	L4SC_TRACE(logger,
+		"%s(\"%s\", pos %ld, \"%s\", expected \"%s\"): rc %d",
+		"PASS", s1, (long) pos1, s2, expected, rc);
+
+	return (BFC_SUCCESS);
+}
+
+
+int
+main(int argc, char *argv[])
+{
+	l4sc_configure_from_xml_file("log4j.xml");
+	logger = l4sc_get_logger(BFC_STRING_LOGGER);
+	pool = bfc_get_stdc_mempool(__FILE__, __LINE__, __FUNCTION__);
+
+#define S1(x) x
+#define S2(x) x
+#define SE(x) x
+	test(S1(""), 0, S2(""), SE(""));
+	test(S1(""), 0, S2("12345"), SE("12345"));
+	test(S1(""), 0, S2("1234567890"), SE("1234567890"));
+	test(S1(""), 0, S2("12345678901234567890"), SE("12345678901234567890"));
+	test(S1(""), 1, S2(""), SE("can't happen"));
+	test(S1(""), 1, S2("12345"), SE("can't happen"));
+	test(S1(""), 1, S2("1234567890"), SE("can't happen"));
+	test(S1(""), 1, S2("12345678901234567890"), SE("can't happen"));
+	test(S1("abcde"), 0, S2(""), SE("abcde"));
+	test(S1("abcde"), 0, S2("12345"), SE("12345abcde"));
+	test(S1("abcde"), 0, S2("1234567890"), SE("1234567890abcde"));
+	test(S1("abcde"), 0, S2("12345678901234567890"), SE("12345678901234567890abcde"));
+	test(S1("abcde"), 1, S2(""), SE("abcde"));
+	test(S1("abcde"), 1, S2("12345"), SE("a12345bcde"));
+	test(S1("abcde"), 1, S2("1234567890"), SE("a1234567890bcde"));
+	test(S1("abcde"), 1, S2("12345678901234567890"), SE("a12345678901234567890bcde"));
+	test(S1("abcde"), 2, S2(""), SE("abcde"));
+	test(S1("abcde"), 2, S2("12345"), SE("ab12345cde"));
+	test(S1("abcde"), 2, S2("1234567890"), SE("ab1234567890cde"));
+	test(S1("abcde"), 2, S2("12345678901234567890"), SE("ab12345678901234567890cde"));
+	test(S1("abcde"), 4, S2(""), SE("abcde"));
+	test(S1("abcde"), 4, S2("12345"), SE("abcd12345e"));
+	test(S1("abcde"), 4, S2("1234567890"), SE("abcd1234567890e"));
+	test(S1("abcde"), 4, S2("12345678901234567890"), SE("abcd12345678901234567890e"));
+	test(S1("abcde"), 5, S2(""), SE("abcde"));
+	test(S1("abcde"), 5, S2("12345"), SE("abcde12345"));
+	test(S1("abcde"), 5, S2("1234567890"), SE("abcde1234567890"));
+	test(S1("abcde"), 5, S2("12345678901234567890"), SE("abcde12345678901234567890"));
+	test(S1("abcde"), 6, S2(""), SE("can't happen"));
+	test(S1("abcde"), 6, S2("12345"), SE("can't happen"));
+	test(S1("abcde"), 6, S2("1234567890"), SE("can't happen"));
+	test(S1("abcde"), 6, S2("12345678901234567890"), SE("can't happen"));
+	test(S1("abcdefghij"), 0, S2(""), SE("abcdefghij"));
+	test(S1("abcdefghij"), 0, S2("12345"), SE("12345abcdefghij"));
+	test(S1("abcdefghij"), 0, S2("1234567890"), SE("1234567890abcdefghij"));
+	test(S1("abcdefghij"), 0, S2("12345678901234567890"), SE("12345678901234567890abcdefghij"));
+	test(S1("abcdefghij"), 1, S2(""), SE("abcdefghij"));
+	test(S1("abcdefghij"), 1, S2("12345"), SE("a12345bcdefghij"));
+	test(S1("abcdefghij"), 1, S2("1234567890"), SE("a1234567890bcdefghij"));
+	test(S1("abcdefghij"), 1, S2("12345678901234567890"), SE("a12345678901234567890bcdefghij"));
+	test(S1("abcdefghij"), 5, S2(""), SE("abcdefghij"));
+	test(S1("abcdefghij"), 5, S2("12345"), SE("abcde12345fghij"));
+	test(S1("abcdefghij"), 5, S2("1234567890"), SE("abcde1234567890fghij"));
+	test(S1("abcdefghij"), 5, S2("12345678901234567890"), SE("abcde12345678901234567890fghij"));
+	test(S1("abcdefghij"), 9, S2(""), SE("abcdefghij"));
+	test(S1("abcdefghij"), 9, S2("12345"), SE("abcdefghi12345j"));
+	test(S1("abcdefghij"), 9, S2("1234567890"), SE("abcdefghi1234567890j"));
+	test(S1("abcdefghij"), 9, S2("12345678901234567890"), SE("abcdefghi12345678901234567890j"));
+	test(S1("abcdefghij"), 10, S2(""), SE("abcdefghij"));
+	test(S1("abcdefghij"), 10, S2("12345"), SE("abcdefghij12345"));
+	test(S1("abcdefghij"), 10, S2("1234567890"), SE("abcdefghij1234567890"));
+	test(S1("abcdefghij"), 10, S2("12345678901234567890"), SE("abcdefghij12345678901234567890"));
+	test(S1("abcdefghij"), 11, S2(""), SE("can't happen"));
+	test(S1("abcdefghij"), 11, S2("12345"), SE("can't happen"));
+	test(S1("abcdefghij"), 11, S2("1234567890"), SE("can't happen"));
+	test(S1("abcdefghij"), 11, S2("12345678901234567890"), SE("can't happen"));
+	test(S1("abcdefghijklmnopqrst"), 0, S2(""), SE("abcdefghijklmnopqrst"));
+	test(S1("abcdefghijklmnopqrst"), 0, S2("12345"), SE("12345abcdefghijklmnopqrst"));
+	test(S1("abcdefghijklmnopqrst"), 0, S2("1234567890"), SE("1234567890abcdefghijklmnopqrst"));
+	test(S1("abcdefghijklmnopqrst"), 0, S2("12345678901234567890"), SE("12345678901234567890abcdefghijklmnopqrst"));
+	test(S1("abcdefghijklmnopqrst"), 1, S2(""), SE("abcdefghijklmnopqrst"));
+	test(S1("abcdefghijklmnopqrst"), 1, S2("12345"), SE("a12345bcdefghijklmnopqrst"));
+	test(S1("abcdefghijklmnopqrst"), 1, S2("1234567890"), SE("a1234567890bcdefghijklmnopqrst"));
+	test(S1("abcdefghijklmnopqrst"), 1, S2("12345678901234567890"), SE("a12345678901234567890bcdefghijklmnopqrst"));
+	test(S1("abcdefghijklmnopqrst"), 10, S2(""), SE("abcdefghijklmnopqrst"));
+	test(S1("abcdefghijklmnopqrst"), 10, S2("12345"), SE("abcdefghij12345klmnopqrst"));
+	test(S1("abcdefghijklmnopqrst"), 10, S2("1234567890"), SE("abcdefghij1234567890klmnopqrst"));
+	test(S1("abcdefghijklmnopqrst"), 10, S2("12345678901234567890"), SE("abcdefghij12345678901234567890klmnopqrst"));
+	test(S1("abcdefghijklmnopqrst"), 19, S2(""), SE("abcdefghijklmnopqrst"));
+	test(S1("abcdefghijklmnopqrst"), 19, S2("12345"), SE("abcdefghijklmnopqrs12345t"));
+	test(S1("abcdefghijklmnopqrst"), 19, S2("1234567890"), SE("abcdefghijklmnopqrs1234567890t"));
+	test(S1("abcdefghijklmnopqrst"), 19, S2("12345678901234567890"), SE("abcdefghijklmnopqrs12345678901234567890t"));
+	test(S1("abcdefghijklmnopqrst"), 20, S2(""), SE("abcdefghijklmnopqrst"));
+	test(S1("abcdefghijklmnopqrst"), 20, S2("12345"), SE("abcdefghijklmnopqrst12345"));
+	test(S1("abcdefghijklmnopqrst"), 20, S2("1234567890"), SE("abcdefghijklmnopqrst1234567890"));
+	test(S1("abcdefghijklmnopqrst"), 20, S2("12345678901234567890"), SE("abcdefghijklmnopqrst12345678901234567890"));
+	test(S1("abcdefghijklmnopqrst"), 21, S2(""), SE("can't happen"));
+	test(S1("abcdefghijklmnopqrst"), 21, S2("12345"), SE("can't happen"));
+	test(S1("abcdefghijklmnopqrst"), 21, S2("1234567890"), SE("can't happen"));
+	test(S1("abcdefghijklmnopqrst"), 21, S2("12345678901234567890"), SE("can't happen"));
+
+	/* pos counts bytes, not utf-8 characters: 2 positions each umlaut */
+	test(S1("\303\244bcdefghijklmn\303\266pqrst\303\274"), 10, S2("12345"), SE("\303\244bcdefghi12345jklmn\303\266pqrst\303\274"));
+
+	test(S1("bcdefghijklmnopqrst"), 0, S2("\344"),
+	     SE("\303\244bcdefghijklmnopqrst"));
+
+	test(S1("abcdefghijklmnpqrst"), 14, S2("\366"),
+	     SE("abcdefghijklmn\303\266pqrst"));
+
+	test(S1("abcdefghijklmnopqrst"), 20, S2("\374"),
+	     SE("abcdefghijklmnopqrst\303\274"));
+
+	test(S1("abcdeklmnopqrst"), 5, S2("\203ghij"),
+	     SE("abcde\306\222ghijklmnopqrst"));
+
+	return (0);
+}

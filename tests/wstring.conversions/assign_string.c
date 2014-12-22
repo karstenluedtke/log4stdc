ae06636d4f2ce46ade7ba75946decb61da6db0f4
Karsten Lüdtke <k.luedtke@teles.de>
2014-12-22 13:07:48 +0100

    testing conversions from different codepages to wstring

--- tests/wstring.conversions/assign_string.c
+++ tests/wstring.conversions/assign_string.c
@@ -18,7 +18,7 @@ static struct mempool *pool;
 static l4sc_logger_ptr_t logger;
 
 static int
-test(const wchar_t *s1, const char *s2, const wchar_t *expected)
+test(const wchar_t *s1, const char *s2, int codepage, const wchar_t *expected)
 {
 	int rc;
 	bfc_string_t s, rs;
@@ -35,8 +35,8 @@ test(const wchar_t *s1, const char *s2, const wchar_t *expected)
 	exp8buf = alloca(bufsize);
 	bfc_utf8_from_wchar(exp8buf, bufsize, expected, rslen);
 
-	L4SC_TRACE(logger, "%s(L\"%s\", \"%s\", expected L\"%s\")",
-		__FUNCTION__, u8buf, s2, exp8buf);
+	L4SC_TRACE(logger, "%s(L\"%s\", \"%s\", %d, expected L\"%s\")",
+		__FUNCTION__, u8buf, s2, codepage, exp8buf);
 
 	rc = bfc_init_basic_wstring_c_str(&s, sizeof(s), pool, s1);
 	assert(rc >= 0);
@@ -44,7 +44,26 @@ test(const wchar_t *s1, const char *s2, const wchar_t *expected)
 	origlen = bfc_string_length(&s);
 	assert(origlen == wcslen(s1));
 	
-	rc = bfc_init_shared_string_c_str(&rs, sizeof(rs), s2);
+	switch (codepage) {
+	case 1:
+		rc = bfc_init_shared_iso8859_1_string_c_str(&rs,sizeof(rs),s2);
+		break;
+	case 2:
+		rc = bfc_init_shared_iso8859_2_string_c_str(&rs,sizeof(rs),s2);
+		break;
+	case 15:
+		rc = bfc_init_shared_iso8859_15_string_c_str(&rs,sizeof(rs),s2);
+		break;
+	case 1250:
+		rc = bfc_init_shared_cp1250_string_c_str(&rs, sizeof(rs), s2);
+		break;
+	case 1252:
+		rc = bfc_init_shared_cp1252_string_c_str(&rs, sizeof(rs), s2);
+		break;
+	default:
+		/* UTF-8 */
+		rc = bfc_init_shared_string_c_str(&rs, sizeof(rs), s2);
+	}
 	assert(rc >= 0);
 
 	rslen = bfc_string_length(&rs);
@@ -66,8 +85,8 @@ test(const wchar_t *s1, const char *s2, const wchar_t *expected)
 	bfc_destroy(&s);
 	bfc_destroy(&rs);
 
-	L4SC_TRACE(logger, "%s(L\"%s\", \"%s\", expected L\"%s\"): rc %d",
-			"PASS", u8buf, s2, exp8buf, rc);
+	L4SC_TRACE(logger, "%s(L\"%s\", \"%s\", %d, expected L\"%s\"): rc %d",
+			"PASS", u8buf, s2, codepage, exp8buf, rc);
 
 	return (BFC_SUCCESS);
 }
@@ -82,36 +101,96 @@ main(int argc, char *argv[])
 #define S1(x) L##x
 #define S2(x) x
 #define SE(x) L##x
-	test(S1(""), S2(""), SE(""));
-	test(S1(""), S2("12345"), SE("12345"));
-	test(S1(""), S2("1234567890"), SE("1234567890"));
-	test(S1(""), S2("12345678901234567890"), SE("12345678901234567890"));
+	test(S1(""), S2(""), 0, SE(""));
+	test(S1(""), S2("12345"), 0, SE("12345"));
+	test(S1(""), S2("1234567890"), 0, SE("1234567890"));
+	test(S1(""), S2("12345678901234567890"), 0, SE("12345678901234567890"));
 
-	test(S1("12345"), S2(""), SE(""));
-	test(S1("12345"), S2("12345"), SE("12345"));
-	test(S1("12345"), S2("1234567890"), SE("1234567890"));
-	test(S1("12345"), S2("12345678901234567890"), SE("12345678901234567890"));
+	test(S1("12345"), S2(""), 0, SE(""));
+	test(S1("12345"), S2("12345"), 0, SE("12345"));
+	test(S1("12345"), S2("1234567890"), 0, SE("1234567890"));
+	test(S1("12345"), S2("12345678901234567890"), 0, SE("12345678901234567890"));
 
-	test(S1("1234567890"), S2(""), SE(""));
-	test(S1("1234567890"), S2("12345"), SE("12345"));
-	test(S1("1234567890"), S2("1234567890"), SE("1234567890"));
-	test(S1("1234567890"), S2("12345678901234567890"), SE("12345678901234567890"));
+	test(S1("1234567890"), S2(""), 0, SE(""));
+	test(S1("1234567890"), S2("12345"), 0, SE("12345"));
+	test(S1("1234567890"), S2("1234567890"), 0, SE("1234567890"));
+	test(S1("1234567890"), S2("12345678901234567890"), 0, SE("12345678901234567890"));
 
-	test(S1("12345678901234567890"), S2(""), SE(""));
-	test(S1("12345678901234567890"), S2("12345"), SE("12345"));
-	test(S1("12345678901234567890"), S2("1234567890"), SE("1234567890"));
-	test(S1("12345678901234567890"), S2("12345678901234567890"),
+	test(S1("12345678901234567890"), S2(""), 0, SE(""));
+	test(S1("12345678901234567890"), S2("12345"), 0, SE("12345"));
+	test(S1("12345678901234567890"), S2("1234567890"), 0, SE("1234567890"));
+	test(S1("12345678901234567890"), S2("12345678901234567890"), 0,
 	     SE("12345678901234567890"));
 
 	do {
 		/* German Umlaute */
 		const char s2[] = "\303\244bcdefghijklmn\303\266pqrst\303\274";
+		const char s8859[] = "\344bcdefghijklmn\366pqrst\374";
 		const wchar_t se[] = { 0xe4, 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 					'i', 'j', 'k', 'l', 'm', 'n', 0xf6,
 					'p', 'q', 'r', 's', 't', 0xfc, 0 };
-		test(S1(""), s2, se);
-		test(S1("12345"), s2, se);
-		test(S1("1234527890123452789012345278901234527890"), s2, se);
+		test(S1(""), s2, 0, se);
+		test(S1("12345"), s2, 0, se);
+		test(S1("1234527890123452789012345278901234527890"), s2, 0, se);
+		test(S1(""), s8859, 1, se);
+		test(S1("12345"), s8859, 1, se);
+		test(S1("123452789012345278901234527890"), s8859, 1, se);
+		test(S1(""), s8859, 2, se);
+		test(S1("12345"), s8859, 2, se);
+		test(S1("123452789012345278901234527890"), s8859, 2, se);
+		test(S1(""), s8859, 15, se);
+		test(S1("12345"), s8859, 15, se);
+		test(S1("123452789012345278901234527890"), s8859, 15, se);
+		test(S1(""), s8859, 1250, se);
+		test(S1("12345"), s8859, 1250, se);
+		test(S1("123452789012345278901234527890"), s8859, 1250, se);
+		test(S1(""), s8859, 1252, se);
+		test(S1("12345"), s8859, 1252, se);
+		test(S1("123452789012345278901234527890"), s8859, 1252, se);
+	} while(0);
+
+	do {
+		/* Euro symbol */
+		const char s2[] = "abcd\342\202\254";
+		const char s8859[] = "abcd\244";
+		const char s1252[] = "abcd\200";
+		const wchar_t se[] = { 'a', 'b', 'c', 'd', 0x20AC, 0 };
+		test(S1(""), s2, 0, se);
+		test(S1("12345"), s2, 0, se);
+		test(S1("1234527890123452789012345278901234527890"), s2, 0, se);
+		test(S1(""), s8859, 15, se);
+		test(S1("12345"), s8859, 15, se);
+		test(S1("123452789012345278901234527890"), s8859, 15, se);
+		test(S1(""), s1252, 1250, se);
+		test(S1("12345"), s1252, 1250, se);
+		test(S1("123452789012345278901234527890"), s1252, 1250, se);
+		test(S1(""), s1252, 1252, se);
+		test(S1("12345"), s1252, 1252, se);
+		test(S1("123452789012345278901234527890"), s1252, 1252, se);
+	} while(0);
+
+	do {
+		/* Eastern European 's' with a 'v' above */
+		const char s2[] = "qr\305\241t";
+		const char s8859_2[] = "qr\271t";
+		const char s8859_15[] = "qr\250t";
+		const char s1252[] = "qr\232t";
+		const wchar_t se[] = { 'q', 'r', 0x0161, 't', 0 };
+		test(S1(""), s2, 0, se);
+		test(S1("12345"), s2, 0, se);
+		test(S1("1234527890123452789012345278901234527890"), s2, 0, se);
+		test(S1(""), s8859_2, 2, se);
+		test(S1("12345"), s8859_2, 2, se);
+		test(S1("123452789012345278901234527890"), s8859_2, 2, se);
+		test(S1(""), s8859_15, 15, se);
+		test(S1("12345"), s8859_15, 15, se);
+		test(S1("123452789012345278901234527890"), s8859_15, 15, se);
+		test(S1(""), s1252, 1250, se);
+		test(S1("12345"), s1252, 1250, se);
+		test(S1("123452789012345278901234527890"), s1252, 1250, se);
+		test(S1(""), s1252, 1252, se);
+		test(S1("12345"), s1252, 1252, se);
+		test(S1("123452789012345278901234527890"), s1252, 1252, se);
 	} while(0);
 
 	if (sizeof(wchar_t) > 2) {
@@ -121,9 +200,9 @@ main(int argc, char *argv[])
 				    0xF0, 0x9F, 0x83, 0x81,
 				    0xF0, 0x9F, 0x83, 0x91, 0 };
 		const wchar_t se[] = { 0x1F0A1, 0x1F0B1, 0x1F0C1, 0x1F0D1, 0 };
-		test(S1(""), s2, se);
-		test(S1("12345"), s2, se);
-		test(S1("1234527890123452789012345278901234527890"), s2, se);
+		test(S1(""), s2, 0, se);
+		test(S1("12345"), s2, 0, se);
+		test(S1("1234527890123452789012345278901234527890"), s2, 0, se);
 	}
 
 	if (sizeof(wchar_t) > 2) {
@@ -136,9 +215,9 @@ main(int argc, char *argv[])
 		 /* D83C */ 0xED, 0xA0, 0xBC, /* DCC1 */ 0xED, 0xB3, 0x81,
 		 /* D83C */ 0xED, 0xA0, 0xBC, /* DCD1 */ 0xED, 0xB3, 0x91, 0 };
 		const wchar_t se[] = { 0x1F0A1, 0x1F0B1, 0x1F0C1, 0x1F0D1, 0 };
-		test(S1(""), s2, se);
-		test(S1("12345"), s2, se);
-		test(S1("1234527890123452789012345278901234527890"), s2, se);
+		test(S1(""), s2, 0, se);
+		test(S1("12345"), s2, 0, se);
+		test(S1("1234527890123452789012345278901234527890"), s2, 0, se);
 	}
 
 	return (0);

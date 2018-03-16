407137def77e80b815af4c462319bdfce27965c0
Karsten Lüdtke <k.luedtke@teles.de>
2018-03-16 10:51:52 +0100

    modified formatting

--- format-sources.sh
+++ format-sources.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-SRCFILES=`find . \( -name \*.c -or -name \*.cpp -or -name \*.h \) -print`
+SRC1=`find log4stdc \( -name \*.c -or -name \*.cpp -or -name \*.h \) -print`
+SRC2=`find test     \( -name \*.c -or -name \*.cpp -or -name \*.h \) -print`
+SRC3=`find examples \( -name \*.c -or -name \*.cpp -or -name \*.h \) -print`
+SRCFILES="$SRC1 $SRC2 $SRC3"
 
 for i in $SRCFILES ; do
 	clang-format $i | sed -f ./macroargs.sed > $i.formatted

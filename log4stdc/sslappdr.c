ce521880cfc4c02cd1596136e26f16060d4d6ff3
Karsten Lüdtke <karsten.luedtke@posteo.de>
2024-06-09 17:58:20 +0200

    sslappender write method

--- log4stdc/sslappdr.c
+++ log4stdc/sslappdr.c
@@ -7,7 +7,17 @@
 
 #include "compat.h"
 
-#if defined(HAVE_OPENSSL3) && (HAVE_OPENSSL - 1 >= 0)
+#if defined(_WIN32) || defined(__MINGW32__) || defined(__MINGW64__)
+#define L4SC_WINDOWS_LOCKS 1
+#include <windows.h>
+#else
+#include <sys/types.h>
+#if defined(HAVE_PTHREAD_H)
+#include <pthread.h>
+#endif
+#endif
+
+#if defined(HAVE_OPENSSL3) && (HAVE_OPENSSL3 - 1 >= 0)
 #include <openssl/ssl.h>
 #include <openssl/bio.h>
 #include <openssl/err.h>
@@ -43,8 +53,6 @@ close_appender(l4sc_appender_ptr_t appender);
 static int
 init_layout(l4sc_appender_ptr_t appender, l4sc_layout_class_ptr_t cls);
 
-#define is_open(appender) ((appender)->fu.fh != NULL)
-
 extern const struct l4sc_appender_class l4sc_socket_appender_class;
 extern const struct l4sc_layout_class l4sc_json_stream_layout_class;
 
@@ -75,14 +83,37 @@ const struct l4sc_appender_class l4sc_ssl_appender_class = {
     /* .ref_layout   */ NULL  /* inherit */
 };
 
+struct ssl_conn {
+    int state;
+#define SSL_CONNECTING   1
+#define SSL_ESTABLISHED  3
+#if defined(HAVE_OPENSSL3) && (HAVE_OPENSSL3 - 1 >= 0)
+    SSL *ssl;
+    SSL_CTX *ctx;
+    BIO *bio;
+#endif
+};
+
+#define sslconn(appender) ((struct ssl_conn *)((appender)->fu.fh))
+#define is_open(appender) (sslconn(appender) && sslconn(appender)->bio)
+
 static int
 init_appender(void *buf, size_t bufsize, bfc_mempool_t pool)
 {
-#if defined(HAVE_OPENSSL3) && (HAVE_OPENSSL - 1 >= 0)
+#if defined(HAVE_OPENSSL3) && (HAVE_OPENSSL3 - 1 >= 0)
     BFC_INIT_PROLOGUE(l4sc_appender_class_ptr_t, l4sc_appender_ptr_t, appender,
                       buf, bufsize, pool, &l4sc_ssl_appender_class);
 
     appender->name = "ssl socket appender";
+#if defined(L4SC_WINDOWS_LOCKS)
+    l4sc_new_win32_mutex(&appender->lock, pool, __FILE__, __LINE__,
+                         "sslappender::init_appender");
+#elif defined(HAVE_PTHREAD_MUTEX_INIT) || defined(__STDC__)
+    l4sc_new_posix_mutex(&appender->lock, pool, __FILE__, __LINE__,
+                         "sslappender::init_appender");
+#else
+    LOGWARN(("sslappender::init_appender: no mutex for appender"));
+#endif
     init_layout(appender, &l4sc_json_stream_layout_class);
     return (BFC_SUCCESS);
 #else
@@ -106,10 +137,29 @@ get_appender_size(l4sc_appender_cptr_t obj)
 #define remotehost filename
 #define remoteport maxbackupindex
 
+static bfc_mutex_ptr_t
+lock_appender(l4sc_appender_ptr_t appender)
+{
+    bfc_mutex_ptr_t lock = appender->lock;
+    if (lock) {
+        bfc_mutex_lock(lock);
+        return (lock);
+    }
+    return (NULL);
+}
+
+static void
+unlock_appender(l4sc_appender_ptr_t appender, bfc_mutex_ptr_t lock)
+{
+    if (lock) {
+        bfc_mutex_unlock(lock);
+    }
+}
+
 static void
 append_to_output(l4sc_appender_ptr_t appender, l4sc_logmessage_cptr_t msg)
 {
-#if defined(HAVE_OPENSSL3) && (HAVE_OPENSSL - 1 >= 0)
+#if defined(HAVE_OPENSSL3) && (HAVE_OPENSSL3 - 1 >= 0)
     if (msg && (msg->msglen > 0)) {
         l4sc_layout_ptr_t layout = &appender->layout;
         bfc_mempool_t pool = appender->parent_pool;
@@ -123,20 +173,41 @@ append_to_output(l4sc_appender_ptr_t appender, l4sc_logmessage_cptr_t msg)
             open_appender(appender);
         }
         if ((len = l4sc_formatmsg(layout, msg, buf, bufsize)) > 0) {
-            if (is_open(appender)) {
-#if 0
-#if defined(L4SC_WINDOWS_SOCKETS)
-                SOCKET sock = get_socket(appender);
-                if (send(sock, buf, len, 0) == -1) {
-                    close_appender(appender);
+            int err, rc = 0;
+            struct ssl_conn *conn = sslconn(appender);
+            if (conn && (conn->state == SSL_ESTABLISHED)) {
+                bfc_mutex_ptr_t lock = lock_appender(appender);
+                if (lock && conn && (conn->state == SSL_ESTABLISHED)) {
+                    BIO *bio = conn->bio;
+                    const char *cp = buf;
+                    int remain = len;
+
+                    while ((remain > 0)
+                        && ((rc = BIO_write(bio, cp, remain)) > 0)) {
+                        cp += rc;
+                        remain -= rc;
+                    }
+
+                    if ((rc < 0) && (remain > 0)) {
+                        err = ERR_get_error();
+                        if (BIO_should_retry(bio)) {
+                            if ((rc = cp - buf) <= 0) {
+                                rc = -EAGAIN;
+                            }
+                        } else {
+                            rc = -EIO;
+                        }
+                    } else {
+                        rc = cp - buf;
+                    }
+
+                    unlock_appender(appender, lock);
                 }
-#else
-                int sock = get_socket(appender);
-                if (send(sock, buf, len, MSG_NOSIGNAL) == -1) {
+                if ((rc < len) && (rc != -EAGAIN)) {
                     close_appender(appender);
+                    LOGWARN(("%s: BIO_write returned %d, err %d.",
+                             "sslappender::append_to_output", rc, err));
                 }
-#endif
-#endif
             }
         }
         if (poolmem) {
@@ -230,3 +301,13 @@ close_appender(l4sc_appender_ptr_t appender)
     }
 #endif
 }
+
+static int
+init_layout(l4sc_appender_ptr_t appender, l4sc_layout_class_ptr_t cls)
+{
+    RETURN_CMETHCALL(
+        l4sc_layout_class_ptr_t, cls, init,
+        (&appender->layout, sizeof(appender->layout), appender->parent_pool),
+        -ENOSYS);
+}
+
